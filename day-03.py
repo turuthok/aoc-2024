4d0123817db3af852b759aab72732c261d80eeed
Lego Haryanto <lego.haryanto@oracle.com>
2024-12-02 21:33:30 -0800

    Day 3

---
+++ day-03.py
@@ -0,0 +1,15 @@
+import re
+
+def generate(s, part):
+    flag = True
+    for x in re.findall(r"(mul[(]\d{1,3},\d{1,3}[)]|don't|do)", s):
+        if x.startswith('do'):
+            flag = (x == 'do')
+        else:
+            if flag or part == 1:
+                a, b = map(int, re.findall(r"\d+", x))
+                yield a * b
+
+s = ''.join(line.strip() for line in open(0))
+print(sum(generate(s, 1)))
+print(sum(generate(s, 2)))
