3fc4b3ce084a5396362c97bfd9d3f353d7250b4c
Lego Haryanto <lego.haryanto@oracle.com>
2024-12-05 10:20:52 -0800

    Revised Day 5 with more whitespaces

--- day-05.py
+++ day-05.py
@@ -1,13 +1,12 @@
 import re
 from functools import cmp_to_key
 
-def ints(iterable):
-    return [int(x) for x in re.findall(r'[-]?\d+', iterable)]
+ints = lambda iterable: [int(x) for x in re.findall(r'[-]?\d+', iterable)]
 
 adj, queries = [[ints(y) for y in x.split()] for x in open(0).read().split('\n\n')]
 adj = {tuple(x) for x in adj}
 
 key = cmp_to_key(lambda a, b: ((b, a) in adj) - ((a, b) in adj))
 
-print(sum(b[len(b)//2] for a in queries if a == (b:= sorted(a, key=key))))
-print(sum(b[len(b)//2] for a in queries if a != (b:= sorted(a, key=key))))
+print(sum(b[len(b) // 2] for a in queries if a == (b := sorted(a, key=key))))
+print(sum(b[len(b) // 2] for a in queries if a != (b := sorted(a, key=key))))
