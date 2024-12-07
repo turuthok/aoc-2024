9460fa22008cd502523a3539d3d7b55d50566005
Lego Haryanto <lego.haryanto@oracle.com>
2024-12-07 08:16:57 -0800

    Day 7, changed to recursion

--- day-07.py
+++ day-07.py
@@ -2,25 +2,22 @@ import re
 
 ints = lambda iterable: list(int(x) for x in re.findall(r'[-]?\d+', iterable))
 
-arr = [x.strip() for x in open(0)]
+arr = [ints(x) for x in open(0)]
 
 def calc(base, arr):
-    tgt, *x = arr
-    n = len(x) - 1
-    for idx in range(base ** n):
-        res = x[0]
-        for i in range(n):
-            d = idx % base
-            idx //= base
-            if d == 0:
-                res *= x[i+1]
-            elif d == 1:
-                res += x[i+1]
-            else:
-                res = int(str(res) + str(x[i+1]))
-        if tgt == res:
-            return tgt
-    return 0
+    tgt, first, *x = arr
+    n = len(x)
 
-print(sum(calc(2, ints(x)) for x in arr))
-print(sum(calc(3, ints(x)) for x in arr))
+    def go(i, val):
+        if val > tgt: return False
+        if i == n: return tgt == val
+        for op in range(base):
+            if op == 0 and go(i+1, val * x[i]): return True
+            if op == 1 and go(i+1, val + x[i]): return True
+            if op == 2 and go(i+1, int(f"{val}{x[i]}")): return True
+        return False
+
+    return tgt if go(0, first) else 0
+
+print(sum(calc(2, x) for x in arr))
+print(sum(calc(3, x) for x in arr))
