ce7a110c613f067183c3586f031286ab5aebeac4
Lego Haryanto <lego.haryanto@oracle.com>
2024-12-12 11:04:30 -0800

    Simplify the edge tracing for Day 12 part 2

--- day-12.py
+++ day-12.py
@@ -2,72 +2,45 @@ from collections import defaultdict
 
 arr = [x.strip() for x in open(0)]
 R, C = len(arr), len(arr[0])
-TOP, RIGHT, BOTTOM, LEFT = 1, 2, 4, 8
-DY, DX, DIR = [-1, 0, 1, 0], [0, 1, 0, -1], [TOP, RIGHT, BOTTOM, LEFT]
+DY, DX = [-1, 0, 1, 0], [0, 1, 0, -1]
 valid = lambda i, j: 0 <= i < R and 0 <= j < C
 
-visited = [[False] * C for _ in range(R)]
+v = [[False] * C for _ in range(R)]
 
-def fill(i, j, char):
-    if not valid(i, j) or visited[i][j] or arr[i][j] != char: return set()
-    visited[i][j] = True
-    res = {(i, j)}
+def fill(r, c, char):
+    if not valid(r, c) or v[r][c] or arr[r][c] != char: return set()
+    v[r][c] = True
+    res = {(r, c)}
     for dy, dx in zip(DY, DX):
-        res |= fill(i+dy, j+dx, char)
+        res |= fill(r+dy, c+dx, char)
     return res
 
-INF = 1_000_000
+def calc(e):
+    last = (-1, -1)
+    res = 0
+    for a, b in e:
+        if a != last[0] or b != last[1]+1: res += 1
+        last = (a, b)
+    return res
 
 def calc_perimeter(cells):
-    l, r, t, b = INF, -INF, INF, -INF
-    data = defaultdict(int)
     perimeter = 0
-    for (i, j) in cells:
-        l = min(l, j); r = max(r, j)
-        t = min(t, i); b = max(b, i)
-        for dy, dx, d in zip(DY, DX, DIR):
-            if (i+dy, j+dx) not in cells:
-                data[(i, j)] |= d
+    edges = defaultdict(list)
+    for r, c in cells:
+        for d, (dy, dx) in enumerate(zip(DY, DX)):
+            rr = r+dy; cc = c+dx
+            if (rr, cc) not in cells:
                 perimeter += 1
-
-    sides = 0
-    for j in range(l, r+1):
-        ll = rr = 0
-        for i in range(t, b+1):
-            if data[(i, j)] & LEFT:
-                if ll == 0:
-                    sides += 1
-                    ll = 1
-            else:
-                ll = 0
-
-            if data[(i, j)] & RIGHT:
-                if rr == 0:
-                    sides += 1
-                    rr = 1
-            else:
-                rr = 0
-    for i in range(t, b+1):
-        bb = tt = 0
-        for j in range(l, r+1):
-            if data[(i, j)] & TOP:
-                if tt == 0:
-                    sides += 1
-                    tt = 1
-            else:
-                tt = 0
-            if data[(i, j)] & BOTTOM:
-                if bb == 0:
-                    sides += 1
-                    bb = 1
-            else:
-                bb = 0
-    return perimeter, sides
+                if d & 1:
+                    edges[d].append((c, r))
+                else:
+                    edges[d].append((r, c))
+    return perimeter, sum(calc(sorted(e)) for e in edges.values())
 
 def gen():
     for i in range(R):
         for j in range(C):
-            if visited[i][j]: continue
+            if v[i][j]: continue
             cells = fill(i, j, arr[i][j])
             perimeter, sides = calc_perimeter(cells)
             yield len(cells), perimeter, sides
