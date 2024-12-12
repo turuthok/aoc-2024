f7505f01c4b95c1a456231ee4f45c73fc5724360
Lego Haryanto <lego.haryanto@oracle.com>
2024-12-12 15:16:51 -0800

    Day 11 modification

--- day-11.py
+++ day-11.py
@@ -1,14 +1,14 @@
 from functools import cache
 
 @cache
-def go(x, n):
-    if n == 0: return 1
-    if x == 0: return go(1, n-1)
+def go(x, steps):
+    if steps == 0: return 1
+    if x == 0: return go(1, steps-1)
     s = str(x)
-    if (l := len(s)) % 2 == 0:
-        l //= 2
-        return go(int(s[:l]), n-1) + go(int(s[l:]), n-1)
-    return go(2024 * x, n-1)
+    if (l := len(s)) & 1:
+        return go(2024 * x, steps-1)
+    else:
+        return sum(go(num, steps-1) for num in divmod(x, 10 ** (l >> 1)))
 
 arr = [int(x) for x in input().split()]
 
