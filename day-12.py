108226138089219946438dffe0e56161519a2bcc
Lego Haryanto <lego.haryanto@oracle.com>
2024-12-12 11:18:21 -0800

    Revised names for Day 12

--- day-12.py
+++ day-12.py
@@ -7,23 +7,23 @@ valid = lambda i, j: 0 <= i < R and 0 <= j < C
 
 v = [[False] * C for _ in range(R)]
 
-def fill(r, c, char):
+def flood_fill(r, c, char):
     if not valid(r, c) or v[r][c] or arr[r][c] != char: return set()
     v[r][c] = True
     res = {(r, c)}
     for dy, dx in zip(DY, DX):
-        res |= fill(r+dy, c+dx, char)
+        res |= flood_fill(r+dy, c+dx, char)
     return res
 
-def calc(e):
+def trace_sides(e):
     last = (-1, -1)
     res = 0
-    for a, b in e:
+    for a, b in sorted(e):
         if a != last[0] or b != last[1]+1: res += 1
         last = (a, b)
     return res
 
-def calc_perimeter(cells):
+def calc_perimeter_and_sides(cells):
     perimeter = 0
     edges = defaultdict(list)
     for r, c in cells:
@@ -35,14 +35,14 @@ def calc_perimeter(cells):
                     edges[d].append((c, r))
                 else:
                     edges[d].append((r, c))
-    return perimeter, sum(calc(sorted(e)) for e in edges.values())
+    return perimeter, sum(trace_sides(e) for e in edges.values())
 
 def gen():
     for i in range(R):
         for j in range(C):
             if v[i][j]: continue
-            cells = fill(i, j, arr[i][j])
-            perimeter, sides = calc_perimeter(cells)
+            cells = flood_fill(i, j, arr[i][j])
+            perimeter, sides = calc_perimeter_and_sides(cells)
             yield len(cells), perimeter, sides
 
 data = list(gen())
