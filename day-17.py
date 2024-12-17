bc30ef24ccb5011182765f3a037e26712b3b69d2
Lego Haryanto <lego.haryanto@oracle.com>
2024-12-17 11:20:23 -0800

    Day 17 yet another revision

--- day-17.py
+++ day-17.py
@@ -1,10 +1,9 @@
 import re
 
 def combo(operand, a, b, c):
-    if operand < 4: return operand
-    return [a, b, c][operand-4]
+    return operand if operand < 4 else [a, b, c][operand-4]
 
-def exec_one(a, b, c):
+def eval_one(a, b, c):
     out = None
     for opcode, operand in zip(instr[::2], instr[1::2]):
         if opcode == 0: a = a >> combo(operand, a, b, c)
@@ -20,19 +19,17 @@ a, b, c, *instr = map(int, re.findall(r"\d+", open(0).read()))
 
 res = []
 while a:
-    opcode, a = exec_one(a, 0, 0)
+    opcode, a = eval_one(a, 0, 0)
     res.append(opcode)
 print(*res, sep=',')
 
-target = instr[::-1]
-
 def go(a, idx):
-    if idx == len(target): return a
-    for check_a in range(8):
-        check_a = a << 3 | check_a
-        out, _ = exec_one(check_a, 0, 0)
-        if out == target[idx]:
-            res = go(check_a, idx+1)
+    if idx < 0: return a
+    for candidate_a in range(8):
+        candidate_a = a << 3 | candidate_a
+        out, _ = eval_one(candidate_a, 0, 0)
+        if out == instr[idx]:
+            res = go(candidate_a, idx-1)
             if res is not None: return res
 
-print(go(0, 0))
+print(go(0, len(instr)-1))
